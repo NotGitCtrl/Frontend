2385ba38cf9f91f1f301a0ed3b0cec94f256f59e
mohit-cfc <santosh.mohit9@gmail.com>
2022-07-31 10:03:21 +0530

    setup react-router

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import "./App.css";
 import SignUpStepper from "./Components/Stepper/Stepper";
 import Login from "./Components/Login/Login";
 import ForgotPass from "./Components/ForgotPassword/ForgotPass";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   // return <ForgotPass />;
-  return <Login />;
+  // return <Login />;
   // return <SignUpStepper />;
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignUpStepper />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
