4a99ad88e42b61c7b2a0090d380eabac47e0ad21
mohit.cfc <santosh.mohit9@gmail.com>
2022-08-26 18:29:53 +0530

    added analytics

--- src/pages/Analytics.js
+++ src/pages/Analytics.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Bar, Pie, Line, Scatter } from "react-chartjs-2";
-import { UserData } from "../utils/fakeData";
+import { UserData, FaData, HeiData, Stream } from "../utils/fakeData";
 import { Chart as ChartJS } from "chart.js/auto";
 import DashboardWrapper from "../Components/common/DashboardWrapper";
 import Box from "@mui/material/Box";
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import Paper from "@mui/material/Paper";
 import { styled } from "@mui/material/styles";
 
@@ -18,11 +18,11 @@ const Item = styled(Paper)(({ theme }) => ({
 
 function Analytics() {
   const [userData, setUserData] = React.useState({
-    labels: UserData.map((data) => data.year),
+    labels: UserData.map((data) => data.state),
     datasets: [
       {
-        label: "Projects Assigned",
-        data: UserData.map((data) => data.userGain),
+        label: "Projects done in various states",
+        data: UserData.map((data) => data.noOfProjects),
         backgroundColor: [
           "rgba(75,192,192,1)",
           "#820000",
@@ -33,14 +33,84 @@ function Analytics() {
       },
     ],
   });
+  const [faData, setFaData] = React.useState({
+    labels: FaData.map((data) => data.funding_agency),
+    datasets: [
+      {
+        label: "Total Amount provided by various funding agencies",
+        data: FaData.map((data) => data.total_amount),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#820000",
+          "#31087B",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+      },
+    ],
+  });
+  const [heiData, setHeiData] = React.useState({
+    labels: HeiData.map((data) => data.HEI),
+    datasets: [
+      {
+        label: "Projects Done in Various HEI's",
+        data: HeiData.map((data) => data.No_of_projects),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#820000",
+          "#31087B",
+          "#f3ba2f",
+          "#2a71d0",
+          "FF7F3F",
+        ],
+      },
+    ],
+  });
+
+  const [streamData, setStreamData] = React.useState({
+    labels: Stream.map((data) => data.stream),
+    datasets: [
+      {
+        label: "Projects Done in Various Streams",
+        data: Stream.map((data) => data.noofProjects),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#820000",
+          "#31087B",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+      },
+    ],
+  });
+  
   return (
     <DashboardWrapper>
       <Box sx={{ width: "100%" }}>
         <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-          <Grid item xs={6}>
+          <Grid item xs={2}></Grid>
+          <Grid marginBottom={5} item xs={8}>
             <Bar data={userData}></Bar>
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={2}></Grid>
+          <Grid item xs={2}></Grid>
+          <Grid marginBottom={5} item xs={8}>
+            <Line data={faData}></Line>
+          </Grid>
+          <Grid item xs={2}></Grid>
+          
+          <Grid item xs={4}></Grid>
+          <Grid marginBottom={5} item xs={4}>
+          <Typography variant="h6">Projects Done in Various HEI's</Typography>
+            <Pie data={heiData}></Pie>
+          </Grid>
+          <Grid item xs={4}></Grid>
+          <Grid item xs={2}></Grid>
+          <Grid marginBottom={5} item xs={8}>
+            <Bar data={streamData}></Bar>
+          </Grid>
+          
+          {/* <Grid item xs={6}>
             <Line data={userData}></Line>
           </Grid>
           <Grid item xs={6}>
@@ -50,7 +120,7 @@ function Analytics() {
           </Grid>
           <Grid item xs={6}>
             <Scatter data={userData}></Scatter>
-          </Grid>
+          </Grid> */}
         </Grid>
       </Box>
     </DashboardWrapper>
--- src/utils/fakeData.js
+++ src/utils/fakeData.js
@@ -1,32 +1,136 @@
-export const UserData = [
+module.exports={
+  UserData : [
+    {
+      id: 1,
+      state: "Gujarat",
+      noOfProjects: 17
+    },
+    {
+      id: 2,
+      state: "Maharashtra",
+      noOfProjects: 28
+    },
   {
-    id: 1,
-    year: 2016,
-    userGain: 80000,
-    userLost: 823,
+      id: 3,
+      state: "UttarPradesh",
+      noOfProjects: 9
   },
   {
-    id: 2,
-    year: 2017,
-    userGain: 45677,
-    userLost: 345,
+      id: 4,
+      state: "Rajasthan",
+      noOfProjects: 23
   },
   {
-    id: 3,
-    year: 2018,
-    userGain: 78888,
-    userLost: 555,
+      id: 5,
+      state: "Tamilnadu",
+      noOfProjects: 11
+  },
+  ],
+  FaData: [
+    {
+      "id": 1,
+      "funding_agency": "JRD.Tata",
+      "total_amount": "200000",
+      "state": "Maharashtra",
+      "year": 2018
   },
   {
-    id: 4,
-    year: 2019,
-    userGain: 90000,
-    userLost: 4555,
+      "id": 2,
+      "funding_agency": "Reliance industries",
+      "total_amount": "275000",
+      "state": "HimachalPradesh",
+      "year": 2020
   },
   {
-    id: 5,
-    year: 2020,
-    userGain: 4300,
-    userLost: 234,
+      "id": 3,
+      "funding_agency": "Adani Wilmer LTD",
+      "total_amount": "299999",
+      "state": "Gujarat",
+      "year": 2022
   },
-];
+  {
+      "id": 4,
+      "funding_agency": "Jio platforms",
+      "total_amount": "180000",
+      "state": "Tamilnadu",
+      "year": 2016
+  },
+  {
+      "id": 5,
+      "funding_agency": "Parle Ingros",
+      "total_amount": "176000",
+      "state": "Karnataka",
+      "year": 2015
+  },
+  ],
+  HeiData: [
+    {
+      "id": 1,
+      "state": "Tamil Nadu",
+      "HEI": "IIT ",
+      "No_of_projects": 10,
+      "Year" : 2020
+  },
+  {
+      "id": 2,
+      "state": "Tamil Nadu",
+      "HEI": "VIT",
+      "No_of_projects": 20,
+      "Year" : 2021
+  },{
+      "id": 3,
+      "state": "Tamil Nadu",
+      "HEI": "NIT",
+      "No_of_projects": 30,
+      "Year" : 2019
+  },
+  {
+      "id": 4,
+      "state": "Tamil Nadu",
+      "HEI": "IIM",
+      "No_of_projects": 90,
+      "Year" : 2017
+  },
+  {
+      "id": 5,
+      "state": "Tamil Nadu",
+      "HEI": "INI",
+      "No_of_projects": 12,
+      "Year" : 2018
+  },
+  {
+      "id": 6,
+      "state": "Tamil Nadu",
+      "HEI": "VJTI",
+      "No_of_projects": 23,
+      "Year" : 2016
+  },
+  ],
+  Stream: [
+    {
+      "id": 1,
+      "stream": "CSE",
+      "noofProjects": 431
+  },
+  {
+      "id": 2,
+      "stream": "Electronics",
+      "noofProjects": 188
+  },{
+      "id": 3,
+      "stream": "IT",
+      "noofProjects": 331
+  },
+  {
+      "id": 4,
+      "stream": "AI&DS",
+      "noofProjects": 444
+  },
+  {
+      "id": 5,
+      "stream": "Civil",
+      "noofProjects": 111
+  },
+  ]
+
+} 
